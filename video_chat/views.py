0e703fce1b87d4be18cfc32c93ac5ca4b688c278
artrsyf <rom.fadeev2017@yandex.ru>
2023-08-31 18:13:00 +0300

    sync load channel page

--- video_chat/views.py
+++ video_chat/views.py
@@ -76,4 +76,15 @@ def get_token(request):
     return JsonResponse({'user_id': user_id, 'token': token, 'stream_id': stream_id, 'stream_token': stream_token}, safe=False)
 
 def channel(request, channel_id):
-    return render(request, 'video_chat/channel.html')
+    # Написать управление контектом, данный код излишен
+    current_user_id = request.user.id
+    current_profile = Profile.objects.get(user=current_user_id)
+    friendship = Friendship.objects.filter(Q(sender=current_profile, status_type=3) | Q(receiver=current_profile, status_type=3))
+    friends = []
+    for relation in friendship:
+        friends.append(relation.receiver if relation.sender.id == current_profile.id \
+                              else relation.sender)
+    context = {
+        'friends': friends
+    }
+    return render(request, 'video_chat/channel.html', context)
