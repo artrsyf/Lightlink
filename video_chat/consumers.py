862b715974d1f0089a2df7a4633168381477e50f
artrsyf <rom.fadeev2017@yandex.ru>
2023-10-07 14:43:18 +0300

    implement consumer class for notifications

--- video_chat/consumers.py
+++ video_chat/consumers.py
@@ -42,6 +42,27 @@ class ChatConsumer(AsyncWebsocketConsumer):
 
         await self.send(text_data=json.dumps({"profile_name": profile_name, "message": message}))
 
+class NotificationConsumer(AsyncWebsocketConsumer):
+    async def connect(self):
+        self.room_name = self.scope["url_route"]["kwargs"]["room_name"]
+        self.room_group_name = f"notification_{self.room_name}"
+
+        await self.channel_layer.group_add(self.room_group_name, self.channel_name)
+
+        await self.accept()
+
+    async def disconnect(self, close_code):
+        await self.channel_layer.group_discard(self.room_group_name, self.channel_name)
+
+    async def receive(self, text_data):
+        text_data_json = json.loads(text_data)
+        
+        await self.channel_layer.group_send(
+            self.room_group_name, text_data_json
+        )
+    
+    
+
 class FriendRequestConsumer(AsyncWebsocketConsumer):
     async def connect(self):
         self.room_name = self.scope["url_route"]["kwargs"]["friend_username"]
--- video_chat/routing.py
+++ video_chat/routing.py
@@ -4,5 +4,6 @@ from . import consumers
 
 websocket_urlpatterns = [
     re_path(r"ws/chat/(?P<room_name>\w+)/$", consumers.ChatConsumer.as_asgi()),
-    re_path(r"ws/service/friend-request/(?P<friend_username>\w+)/$", consumers.FriendRequestConsumer.as_asgi())
+    re_path(r"ws/service/friend-request/(?P<friend_username>\w+)/$", consumers.FriendRequestConsumer.as_asgi()),
+    re_path(r"ws/service/notification/(?P<room_name>\w+)/$", consumers.NotificationConsumer.as_asgi())
 ]
