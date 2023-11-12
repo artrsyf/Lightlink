61b5019ae6c77e970d0f0ff22e3be8bdbb6eebe1
artrsyf <rom.fadeev2017@yandex.ru>
2023-11-12 23:15:19 +0300

    put db engine in .env file

--- lightlink/settings.py
+++ lightlink/settings.py
@@ -154,7 +154,7 @@ CHANNEL_LAYERS = {
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'ENGINE': env("DB_ENGINE"),
         'NAME': env("DB_NAME"),
         'USER': env("DB_USER"),
         'PASSWORD': env("DB_PASSWORD"),
