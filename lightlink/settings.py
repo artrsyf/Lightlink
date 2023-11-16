920f84c05009bcb903805fdca3f1045081921dfe
artrsyf <rom.fadeev2017@yandex.ru>
2023-11-16 23:54:36 +0300

    basic redis cache preset

--- lightlink/settings.py
+++ lightlink/settings.py
@@ -148,6 +148,16 @@ CHANNEL_LAYERS = {
     },
 }
 
+CACHES = {
+    'default': {
+        'BACKEND': 'django_redis.cache.RedisCache',
+        'LOCATION': f'redis://{env("REDIS_HOST")}:{env("REDIS_PORT")}/{env("REDIS_CACHE_DB_ID")}',
+        'OPTIONS': {
+            'CLIENT_CLASS': 'django_redis.client.DefaultClient',
+            'DB': env("REDIS_CACHE_DB_ID")
+        }
+    }
+}
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
