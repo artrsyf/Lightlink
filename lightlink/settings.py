acf4401dacb38fd4324accad7fb29add71626a97
artrsyf <rom.fadeev2017@yandex.ru>
2023-11-20 18:21:54 +0300

    Apply changes from 'dev' into 'master'

--- lightlink/settings.py
+++ lightlink/settings.py
@@ -27,9 +27,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = env("DEBUG")
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = env("ALLOWED_HOSTS").split(' ')
 
 # Emailing settings
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
