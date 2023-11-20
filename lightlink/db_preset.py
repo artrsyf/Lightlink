1dccb468aa6d283ef0f129c843d96555a6bd6be5
artrsyf <rom.fadeev2017@yandex.ru>
2023-11-20 20:55:37 +0300

    Apply changes from 'dev' into 'master'

---
+++ lightlink/db_preset.py
@@ -0,0 +1,31 @@
+import psycopg2
+import environ
+
+env = environ.Env()
+environ.Env().read_env()
+
+def executSqlScript(sql_file_path):
+    try:
+        conn = psycopg2.connect(
+            dbname=env("POSTGRES_DB"),
+            user=env("POSTGRES_USER"),
+            password=env("POSTGRES_PASSWORD"),
+            host=env("POSTGRES_CONTAINER_SERVICE")
+        )
+
+        cur = conn.cursor()
+
+        with open(sql_file_path, 'r') as file:
+            sql_script = file.read()
+
+        cur.execute(sql_script)
+        conn.commit()
+
+        cur.close()
+        conn.close()
+    except Exception as ex:
+        print(f"*SERVER RESPONSE: Failed to execute dafault domain preset in database - {ex}")
+    else:
+        print(f"*SERVER RESPONSE: Succesfully checked database preset")
+
+executSqlScript(env("SQL_PRESET_FILE_PATH"))
