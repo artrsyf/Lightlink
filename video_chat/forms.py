9de686fae0e4430b11a1fb1c4cb2d37468599567
artrsyf <rom.fadeev2017@yandex.ru>
2023-12-11 14:48:41 +0300

    ended with friend request window

--- video_chat/forms.py
+++ video_chat/forms.py
@@ -58,18 +58,11 @@ class FriendshipForm(ModelForm):
     class Meta:
         model = Friendship
         fields = ['receiver_username']
-        widgets = {
-            "receiver_username": TextInput(attrs={
-                'class': 'form-control',
-                'placeholder': 'Имя пользователя'
-            })
-        }
 
     def __init__(self, *args, sender=None, **kwargs):
         super().__init__(*args, **kwargs)
         user = User.objects.get(username=sender)
         self.sender = Profile.objects.get(user=user)
-        # print('init', sender)
 
     def clean(self):
         self.cleaned_data = super().clean()
